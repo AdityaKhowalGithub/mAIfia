e86c8bbfeb1aed58a6f2c0679ba570d62242a429
Nikhil <nikhildevs.108@gmail.com>
2024-10-12 18:51:49 -0700

    123456787654567

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import CharacterBar from "./components/CharacterBar";
+import EventDisplay from "./components/EventDisplay";
 
 function App() {
   return (
     <div className="App">
+      <EventDisplay />
       <CharacterBar />
       {/* Add other components or content for your game here */}
     </div>
